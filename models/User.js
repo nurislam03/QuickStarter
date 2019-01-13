345260b8c416914a42677414e77ed840836a3b02
Nur Islam <nurislam0333@gmail.com>
2019-01-13 09:34:20 +0600

    User schema added

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    defaule: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
